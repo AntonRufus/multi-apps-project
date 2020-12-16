641e2528a86afcf7b4cabc54a14efbeb70fe5809
AntonRufus <zhyvotovskyi@gmail.com>
2020-12-16 13:24:31 +0800

    Dec_16_#1a

--- src/components/pomodoro_timer/PomodoroTimer.jsx
+++ src/components/pomodoro_timer/PomodoroTimer.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 import css from "./PomodoroTimer.modul.css"
 import {Route} from "react-router-dom";
 import App from "../../App";
@@ -8,20 +8,49 @@ function padTime(time) {
 }
 
 const PomodoroTimer = props => {
+    //Hooks
     const [title, setTitle] = useState('Let the countdown begin!');
-    const [timeLeft, setTimeLeft] = useState(0.085 * 60);
+    const [timeLeft, setTimeLeft] = useState(0 * 60);
+    const [isRunning, setIsRunning] = useState(false);
+    const intervalRef = useRef(null);
 
+    //Start timer function
     function startTimer() {
-        setInterval(() => {
+        if (intervalRef.current !== null) return;
+        setTitle('Started!');
+        setIsRunning(true);
+        intervalRef.current = setInterval(() => {
             setTimeLeft(timeLeft => {
                 if (timeLeft >= 1) return timeLeft - 1;
 
                 //reset the timer
+                resetTimer();
+                alert('Time is over'); // why 2 times????
                 return 0;
             });
         }, 1000);
     }
 
+    //Stop timer function
+    function stopTimer() {
+        if (intervalRef.current == null) return;
+
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+        setTitle('Stopped!');
+        setIsRunning(false);
+    }
+
+    //Reset timer function
+    function resetTimer() {
+        setTitle('Ready to go one more round?');
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+        setIsRunning(false);
+        setTimeLeft(0 * 60);
+    }
+
+    //Math
     const minutes = padTime(Math.floor(timeLeft / 60));
     const seconds = padTime(Math.floor(timeLeft - minutes * 60));
 
@@ -29,15 +58,20 @@ const PomodoroTimer = props => {
         <div className={css.wrapper}>
             <Route exact path='/home' render={() => <App/>}/>
             <h2>{title}</h2>
+            <div>
+                {/*avoid negative numbers*/}
+                {!isRunning &&
+                <input defaultValue={timeLeft} onChange={event => setTimeLeft(event.target.value)} type="number"/>}
+            </div>
             <div className={css.timer}>
                 <span>{minutes}</span>
                 <span>:</span>
                 <span>{seconds}</span>
             </div>
             <div className={css.buttons}>
-                <button onClick={startTimer}>Start</button>
-                <button>Stop</button>
-                <button>Reset</button>
+                {!isRunning && <button style={{backgroundColor: 'green'}} onClick={startTimer}>Start</button>}
+                {isRunning && <button style={{backgroundColor: 'orange'}} onClick={stopTimer}>Stop</button>}
+                <button style={{backgroundColor: 'red'}} onClick={resetTimer}>Reset</button>
             </div>
         </div>
     )
