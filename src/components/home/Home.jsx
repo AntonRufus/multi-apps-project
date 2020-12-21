ef15792db3a4b59c85b1d85c2e4103bb76940d42
AntonRufus <zhyvotovskyi@gmail.com>
2020-12-21 17:49:06 +0800

    Dec_21_#6 Home jsx & css

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import css from './Home.module.css';
+import React from "react";
+
+function App() {
+    return (
+        <div className={css.home}>
+            Hi There!
+        </div>
+    );
+}
+
+export default App;
