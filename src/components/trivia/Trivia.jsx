a9c18496eca970c52baa7cf1433b17a668b9fc2f
AntonRufus <zhyvotovskyi@gmail.com>
2021-01-25 19:04:36 +0800

    Jan_25_#2 Scoreboard + css

--- src/components/trivia/Trivia.jsx
+++ src/components/trivia/Trivia.jsx
@@ -1,45 +1,64 @@
-import React, {useEffect, useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import css from "./Trivia.module.css"
 import CategorySelector from "./components/CategorySelector";
 import Scoreboard from "./components/Scoreboard";
 import Question from "./components/Question";
+import ResultModal from "./components/ResultModal";
 
 const Trivia = () => {
     const [question, setQuestion] = useState(null);
+    const [selectedCategory, setSelectedCategory] = useState('any');
+    const [isCorrect, setIsCorrect] = useState(null);
+    // const [wrongScore, setWrongScore] = useState(0);
+    // const [correctScore, setCorrectScore] = useState(0);
 
-    useEffect(() => {
-        getQuestion();
-    }, []);
-
-    const getQuestion = () => {
-        const url = 'https://opentdb.com/api.php?amount=1';
+    const getQuestion = useCallback(() => {
+        setIsCorrect(null);
+        let url = 'https://opentdb.com/api.php?amount=1';
+        if (selectedCategory !== 'any') url += `&category=${selectedCategory}`;
+        console.log(url);
 
         fetch(url)
             .then(res => res.json())
             .then(data => setQuestion(data.results[0]));
+    }, [selectedCategory]);
+
+    const handleQuestionAnswered = (answer) => {
+        const isAnswerCorrect = answer === question.correct_answer;
+        setIsCorrect(isAnswerCorrect);
+        console.log(isCorrect);
+
+        // if (isAnswerCorrect) setCorrectScore((score) => score + 1);
+        // else setWrongScore((score) => score + 1);
     };
+
+    useEffect(() => {
+        getQuestion();
+    }, [getQuestion, selectedCategory]);
 
     return (
         <div className={css.wrapper}>
 
             <div className={css.app}>
                 {/* show the result modal ----------------------- */}
-                {/* <ResultModal /> */}
-
+                {isCorrect !== null &&
+                <ResultModal correctAnswer={question.correct_answer} isCorrect={isCorrect} getQuestion={getQuestion}
+                             setIsCorrect={setIsCorrect}/>}
                 {/* question header ----------------------- */}
                 <div className={css.question_header}>
-                    <CategorySelector/>
-                    <Scoreboard/>
+                    <CategorySelector chooseCategory={setSelectedCategory} category={selectedCategory}/>
+                    {/*<Scoreboard wrong={wrongScore} correct={correctScore}/>*/}
+                    <Scoreboard isCorrect={isCorrect}/>
                 </div>
 
                 {/* the question itself ----------------------- */}
                 <div className={css.question_main}>
-                    {question && <Question question={question}/>}
+                    {question && <Question question={question} answerQuestion={handleQuestionAnswered}/>}
                 </div>
 
                 {/* question footer ----------------------- */}
                 <div className={css.question_footer}>
-                    <button>Go to next question 👉</button>
+                    <button onClick={getQuestion}>Go to next question</button>
                 </div>
             </div>
 
--- src/components/trivia/components/Scoreboard.js
+++ src/components/trivia/components/Scoreboard.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import css from "../Trivia.module.css"
+import {useAlert} from "react-alert";
 
-export default function Scoreboard() {
+export default function Scoreboard({isCorrect}) {
+    const [wrong, setWrong] = useState(0);
+    const [correct, setCorrect] = useState(0);
+    const alert = useAlert();
+
+    useEffect(() => {
+        if (isCorrect === null) return;
+
+        if (isCorrect) setCorrect((score) => score + 1);
+        else setWrong((score) => score + 1);
+    }, [isCorrect]);
+
+    useEffect(() => {
+        if (wrong === 1)
+            alert.show('Keep pushing 🤦‍♂🤷'); // why 2 times???
+    }, [wrong]);
+
+    useEffect(() => {
+        if (correct === 1)
+            alert.show('Good job 🤪💥💪');
+    }, [correct]);
+
     return (
         <div className={css.scoreboard}>
             <div className={css.wrong}>
-                <strong>0</strong>
+                <strong>{wrong}</strong>
                 <span>wrong</span>
             </div>
             <div className={css.correct}>
-                <strong>0</strong>
+                <strong>{correct}</strong>
                 <span>correct</span>
             </div>
         </div>
