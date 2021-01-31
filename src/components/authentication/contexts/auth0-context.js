5509f60548aedd8477d328cbb11fbcf701068c6c
AntonRufus <zhyvotovskyi@gmail.com>
2021-01-31 15:50:15 +0800

    Jan_31 #9

--- src/components/authentication/contexts/auth0-context.js
+++ src/components/authentication/contexts/auth0-context.js
@@ -22,8 +22,9 @@ export function Auth0Provider({children}) {
             const auth0 = await createAuth0Client({
                 domain: 'antonrufus.eu.auth0.com',
                 client_id: 'GDDE7o0r59yoAoX9nufbSWDr63hNEotG',
-                redirect_uri: window.location.origin,
+                // redirect_uri: window.location.origin,
                 // redirect_uri: window.location.href='https://antonrufus.github.io/multi-apps-project/#/authentication',
+                redirect_uri: window.location.href='https://antonrufus.github.io/multi-apps-project/',
             },
 
                 console.log(window.location.origin)
