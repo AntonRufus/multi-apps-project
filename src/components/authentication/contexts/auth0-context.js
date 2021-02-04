a6e682f91ea7df7a8271f8118c9da6bdb47be36b
AntonRufus <zhyvotovskyi@gmail.com>
2021-02-04 15:14:22 +0800

    Feb_4 #3

--- src/components/authentication/contexts/auth0-context.js
+++ src/components/authentication/contexts/auth0-context.js
@@ -20,7 +20,8 @@ export function Auth0Provider({children}) {
                 domain: 'antonrufus.eu.auth0.com',
                 client_id: 'GDDE7o0r59yoAoX9nufbSWDr63hNEotG',
                 // redirect_uri: window.location.origin,       // to "https://antonrufus.github.io/multi-apps-project"
-                redirect_uri: window.location.href = 'https://antonrufus.github.io/multi-apps-project/#/authentication/home',   // to auth app's homepage
+                // redirect_uri: window.location.href = 'https://antonrufus.github.io/multi-apps-project/#/authentication/home',   // to auth app's homepage
+                redirect_uri: window.location.href = 'https://antonrufus.github.io/multi-apps-project/#/home',   // to auth main app's homepage
             });
 
             setAuth0Client(auth0);
@@ -38,6 +39,7 @@ export function Auth0Provider({children}) {
 
                 // window.location.replace(window.location.pathname);                       //!!!!!!
                 window.location.href.replace('https://antonrufus.github.io/multi-apps-project/#/authentication/home');
+                window.location.href.reload('https://antonrufus.github.io/multi-apps-project/#/authentication/home');
             }
 
             // is a user authenticated
