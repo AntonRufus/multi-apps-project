be8464c2fce148be3383bbe2f7bf6abd55bcd514
AntonRufus <zhyvotovskyi@gmail.com>
2021-02-04 15:32:06 +0800

    Feb_4 #6

--- src/components/authentication/contexts/auth0-context.js
+++ src/components/authentication/contexts/auth0-context.js
@@ -38,7 +38,8 @@ export function Auth0Provider({children}) {
                 }
 
                 // window.location.replace(window.location.pathname);                       //!!!!!!
-                window.location.href.replace('https://antonrufus.github.io/multi-apps-project/#/authentication/home');
+                window.location.replace('https://antonrufus.github.io/multi-apps-project/#/authentication/home');
+                window.location.reload('https://antonrufus.github.io/multi-apps-project/#/authentication/home');
             }
 
             // is a user authenticated
