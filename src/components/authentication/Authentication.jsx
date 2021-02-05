15082720faecfb7a68c05ed2a9dc42d5fe558a9f
AntonRufus <zhyvotovskyi@gmail.com>
2021-02-05 13:49:16 +0800

    Feb_5 #5

--- src/components/authentication/Authentication.jsx
+++ src/components/authentication/Authentication.jsx
@@ -5,7 +5,7 @@ import SiteHeader from "./components/SiteHeader";
 import HomeAuth from "./components/HomeAuth";
 import Dashboard from "./components/Dashboard";
 import {useAuth0} from "./contexts/auth0-context";
-import PrivateRoute, {HomeRoute} from "./components/PrivateRoute";
+import PrivateRoute from "./components/PrivateRoute";
 
 const Authentication = () => {
     const {getToken} = useAuth0();
@@ -36,9 +36,9 @@ const Authentication = () => {
                         <PrivateRoute path="/authentication/dashboard">
                             <Dashboard/>
                         </PrivateRoute>
-                        <HomeRoute path="/authentication/home">
+                        <Route path="/authentication/home">
                             <HomeAuth/>
-                        </HomeRoute>
+                        </Route>
                     </Switch>
                 </div>
             </Router>
