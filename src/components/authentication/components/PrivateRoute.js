76afd37b006d68acf1e7707b7bca069c2fc1e5f4
AntonRufus <zhyvotovskyi@gmail.com>
2021-02-05 14:26:49 +0800

    Feb_5 #8

--- src/components/authentication/components/PrivateRoute.js
+++ src/components/authentication/components/PrivateRoute.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {Route} from "react-router-dom";
 import {useAuth0} from "../contexts/auth0-context";
-import Dashboard from "./Dashboard";
 
 export default function PrivateRoute({children, ...rest}) {
     const {isAuthenticated, user, login} = useAuth0();
@@ -9,14 +8,4 @@ export default function PrivateRoute({children, ...rest}) {
     if (!isAuthenticated && !user) login();
 
     return <Route {...rest}>{children}</Route>;
-}
-
-export function HomeRoute({children, ...rest}) {
-    const {isAuthenticated, user} = useAuth0();
-
-    if (!isAuthenticated && !user) {
-        return <Route {...rest}>{children}</Route>
-    } else {
-        return <Route  path="/authentication/dashboard"><Dashboard/></Route>
-    }
 }
